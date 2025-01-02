74a613845b098baba8f398e846804bedb41bbfbb
Patrick <petrik09@live.nl>
2025-01-02 10:49:10 +0100

    fix: xp orb anim

--- src/entities/xp-orb.ts
+++ src/entities/xp-orb.ts
@@ -1,11 +1,12 @@
 import { Entity, newEntity, setSprites } from "entity.js";
-import { tickTimer, tween } from "ridder";
+import { random, tickTimer, tween } from "ridder";
 import { destroyEntity, getPlayer, Scene } from "scene.js";
 
 export function newExperienceOrb(scene: Scene, x: number, y: number, experience: number) {
   const e = newEntity(scene, "experience_orb", x, y);
 
   e.stats.experience = experience;
+  e.tweenDuration = random(5, 10) * 100;
 
   setSprites(e, "experience_orb", 8, 10, 0, 0, true, 0, 1);
 
@@ -15,9 +16,9 @@ export function newExperienceOrb(scene: Scene, x: number, y: number, experience:
 export function updateExperienceOrb(e: Entity, scene: Scene) {
   const player = getPlayer(scene);
 
-  const completed = tickTimer(e.tweenTimer, 1000);
-  e.position.x = tween(e.start.x, player.center.x, 500, "easeInCirc", e.tweenTimer.elapsed);
-  e.position.y = tween(e.start.y, player.center.y, 500, "easeInCirc", e.tweenTimer.elapsed);
+  const completed = tickTimer(e.tweenTimer, e.tweenDuration);
+  e.position.x = tween(e.start.x, player.center.x, e.tweenDuration, "easeInCirc", e.tweenTimer.elapsed);
+  e.position.y = tween(e.start.y, player.center.y, e.tweenDuration, "easeInCirc", e.tweenTimer.elapsed);
 
   if (completed) {
     destroyEntity(scene, e);
