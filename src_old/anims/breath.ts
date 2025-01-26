d9ed1749100cba90fcc721baf7610d63177e8f2d
Patrick <petrik09@live.nl>
2025-01-26 18:13:32 +0100

    chore: removed old source

--- src_old/anims/breath.ts
+++
@@ -1,8 +0,0 @@
-import { Entity } from "entity.js";
-import { tickTimer, tween } from "ridder";
-
-export function updateBreathAnimation(e: Entity) {
-  tickTimer(e.tweenTimer, Infinity);
-  e.tweenScale.x = tween(1, 1.1, 2000, "easeInOutSine", e.tweenTimer.elapsed);
-  e.tweenScale.y = tween(1, 1.1, 2000, "easeInOutSine", e.tweenTimer.elapsed);
-}
--- src_old/render.ts
+++
@@ -1,9 +0,0 @@
-import { drawText, TextAlign, TextBaseline } from "ridder";
-
-export function drawOutlinedText(text: string, x: number, y: number, color = "white", outlineColor = "black", align: TextAlign = "left", baseline: TextBaseline = "top") {
-  drawText(text, x, y - 1, outlineColor, align, baseline);
-  drawText(text, x + 1, y, outlineColor, align, baseline);
-  drawText(text, x, y + 1, outlineColor, align, baseline);
-  drawText(text, x - 1, y, outlineColor, align, baseline);
-  drawText(text, x, y, color, align, baseline);
-}
