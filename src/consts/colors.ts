657e251a2b36fc8faa02521868ed7e648216b887
Patrick <petrik09@live.nl>
2025-02-02 19:43:58 +0100

    style: bg color

--- src/consts/colors.ts
+++ src/consts/colors.ts
@@ -1,4 +1,4 @@
-export const COLOR_BG = "#393E41";
+export const COLOR_BG = "#796465";
 export const COLOR_OUTLINE = "#1e1e1e";
 export const COLOR_PRIMARY = "#ffffff";
 export const COLOR_DANGER = "#ff0000";
