6e687a287a1d8d2a256834fb2c94897fce1e71b8
Patrick <petrik09@live.nl>
2025-03-29 12:49:12 +0100

    chore: use env var instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { execSync } from "child_process";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
-
 export default defineConfig({
   define: {
-    __VERSION__: JSON.stringify(`${process.env.npm_package_version} (${commitHash})`),
+    __VERSION__: JSON.stringify(`${process.env.npm_package_version} (${process.env.GITHUB_SHA ?? "dev"})`),
   },
   plugins: [tsconfigPaths()],
 });
