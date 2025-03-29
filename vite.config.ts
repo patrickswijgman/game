ee40fc51527e2d6c6cc9aaad998986085d559e79
Patrick <petrik09@live.nl>
2025-03-29 12:47:16 +0100

    chore: show commit hash as well

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+import { execSync } from "child_process";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+
 export default defineConfig({
   define: {
-    __VERSION__: JSON.stringify(process.env.npm_package_version),
+    __VERSION__: JSON.stringify(`${process.env.npm_package_version} (${commitHash})`),
   },
   plugins: [tsconfigPaths()],
 });
